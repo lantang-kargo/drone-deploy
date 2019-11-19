c6b61bc97715365f575e41974db5352ff7be2c29
Lantang Satriatama <lantang@kargo.tech>
2019-11-19 11:32:58 +0700

    edit payload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 def main():
     webhook_url = os.environ.get('PLUGIN_WEBHOOK_URL')
     s3_bucket = os.environ.get('PLUGIN_S3_BUCKET')
-    data = '{ text: The APK in ' + s3_bucket + ' has been updated. }'
+    data = '{ "text": "The APK in ' + str(s3_bucket) + ' has been updated." }'
     r = requests.post(url = webhook_url, data = data)
     print(r.text)
 
